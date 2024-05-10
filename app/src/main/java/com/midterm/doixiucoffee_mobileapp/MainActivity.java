f201d5e9cb0c9926c3192d2827f1aca4937f4080
Le Viet Thanh <115569080+samuel7232003@users.noreply.github.com>
2024-05-10 09:35:52 +0700

    Thanh

--- app/src/main/java/com/midterm/doixiucoffee_mobileapp/MainActivity.java
+++ app/src/main/java/com/midterm/doixiucoffee_mobileapp/MainActivity.java
@@ -2,13 +2,29 @@ package com.midterm.doixiucoffee_mobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.midterm.doixiucoffee_mobileapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View viewRoot = binding.getRoot();
+        setContentView(viewRoot);
+
+        binding.openBooking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BookingLayout.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
