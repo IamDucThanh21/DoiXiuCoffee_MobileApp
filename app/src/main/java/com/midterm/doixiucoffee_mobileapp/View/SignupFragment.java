38854170f892886b3c6fd75090f6d9cbca1c6922
hi-imchuy <chauhoanghuy562003@gmail.com>
2024-06-06 18:55:53 +0700

    Huy

--- app/src/main/java/com/midterm/doixiucoffee_mobileapp/View/SignupFragment.java
+++ app/src/main/java/com/midterm/doixiucoffee_mobileapp/View/SignupFragment.java
@@ -7,7 +7,9 @@ import android.view.ViewGroup;
 
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import com.midterm.doixiucoffee_mobileapp.Firebase.DataPerson;
 import com.midterm.doixiucoffee_mobileapp.R;
 import com.midterm.doixiucoffee_mobileapp.databinding.FragmentSignupBinding;
 
@@ -28,7 +30,32 @@ public class SignupFragment extends Fragment {
 
         binding = DataBindingUtil.inflate(getLayoutInflater(), R.layout.fragment_signup, null, false);
 
+        binding.signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phone = binding.etPhone.getText().toString().trim();
+                String name = binding.etName.getText().toString().trim();
 
+                if (!DataPerson.getInstance().checkPhone(phone)){
+                    binding.error.setText("Số điện thoại đã được đăng kí");
+                    binding.error.setVisibility(View.VISIBLE);
+                }else if (phone.equals("") || name.equals("")){
+                    binding.error.setText("Vui lòng nhập đầy đủ thông tin");
+                    binding.error.setVisibility(View.VISIBLE);
+                }
+                else {
+                    DataPerson.getInstance().addNewUser(name, phone);
+                    Navigation.findNavController(v).navigate(R.id.loginFragment);
+                }
+            }
+        });
+
+        binding.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.loginFragment);
+            }
+        });
 
         View view = binding.getRoot();
         return view;
