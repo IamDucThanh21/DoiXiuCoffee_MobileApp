e8e66ec172ea08929d71b20bfe0aac0a07413a61
IamDucThanh21 <leducthanh2107@gmail.com>
2024-06-07 16:10:01 +0700

    edit music item

--- app/src/main/java/com/midterm/doixiucoffee_mobileapp/ViewModel/SongAdapter.java
+++ app/src/main/java/com/midterm/doixiucoffee_mobileapp/ViewModel/SongAdapter.java
@@ -4,12 +4,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.imageview.ShapeableImageView;
+import com.midterm.doixiucoffee_mobileapp.Firebase.DataPerson;
 import com.midterm.doixiucoffee_mobileapp.Model.Song;
 import com.midterm.doixiucoffee_mobileapp.R;
 import com.squareup.picasso.Picasso;
@@ -39,6 +42,11 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
         holder.songSinger.setText(listSong.get(position).getSinger());
         holder.songVotes.setText(listSong.get(position).getVotes()+"");
         Picasso.get().load(listSong.get(position).getImage()).into(holder.avaSong);
+        if(DataPerson.getInstance().isAdminById(DataPerson.getInstance().getIdPersonLogin())){
+            holder.layoutPersonHearts.setVisibility(View.GONE);
+            holder.layoutPersonChooses.setVisibility(View.GONE);
+            holder.layoutGarbage.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
@@ -51,12 +59,24 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
         public TextView songSinger;
         public TextView songVotes;
         public ShapeableImageView avaSong;
+        public LinearLayout layoutPersonChooses;
+        public LinearLayout layoutPersonHearts;
+        public LinearLayout layoutGarbage;
+//        public TextView numPersonVotes;
+//        public ImageView personVotes;
+//        public ImageView imvHeart;
+//        public ImageView imvGarbage;
         public ViewHolder(View itemView) {
             super(itemView);
             songName = (TextView) itemView.findViewById(R.id.song_name);
             songSinger = (TextView) itemView.findViewById(R.id.song_singer);
             songVotes = (TextView) itemView.findViewById(R.id.song_votes);
             avaSong = (ShapeableImageView) itemView.findViewById(R.id.song_image);
+//            numPersonVotes = (TextView) itemView.findViewById(R.id.song_votes);
+//            personVotes = (ImageView) itemView.findViewById(R.id.)
+            layoutPersonChooses = (LinearLayout) itemView.findViewById(R.id.person_chooses);
+            layoutPersonHearts = (LinearLayout) itemView.findViewById(R.id.heart_votes);
+            layoutGarbage = (LinearLayout) itemView.findViewById(R.id.garbage);
         }
     }
 }
